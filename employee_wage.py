da325adf89ed7317fffd4baefecc8845c25de860
ravi04ez <ezekie.em@gmail.com>
2022-09-28 07:12:15 +0530

    Added full & part Time Wage

--- employee_wage.py
+++ employee_wage.py
@@ -3,16 +3,20 @@ import random
 
 class Employee:
     def daily_wage(self):
-        is_present = 1
+        is_part_time = 1
+        is_full_time = 2
         emp_rate_per_hrs = 20
         emp_hrs = 0
-        emp_check = random.randrange(0, 2)
-        if emp_check == is_present:
-            print("Employee is Present")
+        emp_check = random.randrange(0, 3)
+        if emp_check == is_full_time:
+            print("Employee is worinking Full TIme")
             emp_hrs = 8
+        elif emp_check == is_part_time:
+            print("Employee is working Part time")
+            emp_hrs = 4
         else:
             print("Employee is Absent")
-            emp_hrs = 0
+
         emp_wage = emp_hrs*emp_rate_per_hrs
         print("Employee Wage : ", emp_wage)
 
