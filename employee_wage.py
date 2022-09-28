ac5117d1a2991cdde428081d37a395dada0e1778
ravi04ez <ezekie.em@gmail.com>
2022-09-28 07:38:11 +0530

    Added Emp Month Wages

--- employee_wage.py
+++ employee_wage.py
@@ -2,25 +2,32 @@ import random
 
 
 class Employee:
-    def daily_wage(self):
+    def emp_mnth_wage(self):
         is_part_time = 1
         is_full_time = 2
         emp_rate_per_hrs = 20
+        num_of_wrkng_days = 20
         emp_hrs = 0
-        emp_check = random.randrange(0, 3)
-        if emp_check == is_full_time:
-            print("Employee is worinking Full TIme")
-            emp_hrs = 8
-        elif emp_check == is_part_time:
-            print("Employee is working Part time")
-            emp_hrs = 4
-        else:
-            print("Employee is Absent")
+        total_emp_wage = 0
 
-        emp_wage = emp_hrs*emp_rate_per_hrs
-        print("Employee Wage : ", emp_wage)
+        for day in range(num_of_wrkng_days):
+            emp_check = random.randrange(0, 3)
+            if emp_check == is_full_time:
+                # print("Employee is worinking Full TIme")
+                emp_hrs = 8
+            elif emp_check == is_part_time:
+                # print("Employee is working Part time")
+                emp_hrs = 4
+            else:
+                # print("Employee is Absent")
+                emp_hrs = 0
+
+            emp_wage = emp_hrs*emp_rate_per_hrs
+            total_emp_wage += emp_wage
+
+        print("Total Employee Wage of a Month :", total_emp_wage)
 
 
 if __name__ == "__main__":
     obj_emp = Employee()
-    obj_emp.daily_wage()
+    obj_emp.emp_mnth_wage()
